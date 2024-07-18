b69dcddb1d44438747b1c23a1f66b3071ccbcb5a
mkiefer3711 <74162397+mkiefer3711@users.noreply.github.com>
2024-07-18 14:47:02 -0400

    Add files via upload

---
+++ Maddison Kiefer Project 1-1 Restaurant Information/src/RestaurantInformation.java
@@ -0,0 +1,11 @@
+//@author Maddison Kiefer
+
+// Create the main class
+public class RestaurantInformation {
+    public static void main(String[] args) {
+        // Output to the screen
+        System.out.println("A. Buddy's Pizza");
+        System.out.println("B. 33605 Plymouth Rd");
+        System.out.println("C. Livonia, MI, 48150");
+    }
+}
